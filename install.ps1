802580bf21912801de64a100d645dd2424bf3a2e
Peter Lisovin <peter@lisovin.com>
2013-12-07 03:07:24 -0500

    Initial revision.

---
+++ install.ps1
@@ -0,0 +1,11 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$project | 
+	Add-Paths "{
+		'less'			: 'Scripts/less',
+		'less-builder'	: 'Scripts/less-builder',
+		'lessc'			: 'Scripts/lessc',
+		'normalize'		: 'Scripts/normalize',
+		'styles'		: 'Scripts/scalejs.styles'
+	}" |
+	Out-Null
